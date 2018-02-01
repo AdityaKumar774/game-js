b568c8114fedb07b70090050e2c97e6d5f710ed5
Aditya Kumar <aditya.kumar774@gmail.com>
2018-02-02 01:51:20 +0530

    adding jquery

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('.pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var score = $('#score');
+    var speed_span = $('#speed');
+    var restart_btn = $('#restart_btn');
+
+    var container_width = parseInt(container.width());
+    var pole_initial_position = parseInt(pole.css('right'));
+    var pole_initial_height = parseInt(pole.css('height'));
+    var bird_left = parseInt(bird.css('left'));
+    var speed = 10;
+    
+})
